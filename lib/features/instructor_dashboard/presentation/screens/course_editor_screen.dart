6361af6c771c2541b00c74cc8c8b3b8a2ee57e44
AHMED\ahmed <ahmed01142043116@gmail.com>
2026-06-23 17:41:47 +0300

    fix: Add missing easy_localization import in course editor

--- lib/features/instructor_dashboard/presentation/screens/course_editor_screen.dart
+++ lib/features/instructor_dashboard/presentation/screens/course_editor_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../core/services/app_logger.dart';
