22796777f4abdcb2cb36261a8425fcf9b2af7e0a
AHMED\ahmed <ahmed01142043116@gmail.com>
2026-07-14 05:48:26 +0300

    chore: Update Google Web Client ID

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -16,7 +16,7 @@ class AppConstants {
   static const String supabaseJwtAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ndWRhbHNjY25yYWd1YWppcWt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NTkxOTksImV4cCI6MjA5OTQzNTE5OX0.umuFbHzz_ZR0OauUhJaUaqzu9ByWmEMHJGOkZ4Vxsro';
   static const String googleWebClientId =
-      '1083887222364-3jlne3f2n5jqdht1nvbp4i0trev8sgca.apps.googleusercontent.com';
+      '901902911640-jq1q4pd0gpdvk2g08dinoaet502jcj8r.apps.googleusercontent.com';
 
   // Storage Buckets
   static const String avatarsBucket = 'avatars';
