5f635c264af7e1f05a4a55cab3842edb0ca70fbe
AHMED\ahmed <ahmed01142043116@gmail.com>
2026-07-14 06:21:43 +0300

    chore: update google client id and revert error handling

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -16,7 +16,7 @@ class AppConstants {
   static const String supabaseJwtAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ndWRhbHNjY25yYWd1YWppcWt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NTkxOTksImV4cCI6MjA5OTQzNTE5OX0.umuFbHzz_ZR0OauUhJaUaqzu9ByWmEMHJGOkZ4Vxsro';
   static const String googleWebClientId =
-      '901902911640-jq1q4pd0gpdvk2g08dinoaet502jcj8r.apps.googleusercontent.com';
+      '901902911640-alaal9vjbd6d05qiodnhi7fetck05d5b.apps.googleusercontent.com';
 
   // Storage Buckets
   static const String avatarsBucket = 'avatars';
